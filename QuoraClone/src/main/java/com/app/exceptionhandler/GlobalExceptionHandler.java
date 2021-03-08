96e2e54c3cf3bd1d523d62739a5b79df2e99a8e9
lenovo <lenovo@DESKTOP-FU1HSC8>
2021-03-08 14:31:23 +0530

    contact_details Exception handling

--- QuoraClone/src/main/java/com/app/exceptionhandler/GlobalExceptionHandler.java
+++ QuoraClone/src/main/java/com/app/exceptionhandler/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.app.customexception.ContactDetailsNotFoundException;
 import com.app.customexception.UserAuthorizationException;
 import com.app.customexception.UserNotFoundException;
 import com.app.dto.ErrorResponse;
@@ -12,6 +13,7 @@ import com.app.dto.ErrorResponse;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+	// exception handler methods : User
 	@ExceptionHandler(UserAuthorizationException.class)
 	public ResponseEntity<?> userExceptionHandler(UserAuthorizationException e) {
 		return new ResponseEntity<>(new ErrorResponse("Unable to authorize user", e.getMessage()),
@@ -23,6 +25,14 @@ public class GlobalExceptionHandler {
 		return new ResponseEntity<>(new ErrorResponse("User not found", e.getMessage()), HttpStatus.NOT_FOUND);
 	}
 	
-	// exception handler methods
-
+	// exception handler methods : contact_Details
+	@ExceptionHandler(ContactDetailsNotFoundException.class)
+	public ResponseEntity<?> contactDetailsExceptionHandler(ContactDetailsNotFoundException e){
+		return new ResponseEntity<>(new ErrorResponse("Contact details not found", e.getMessage()), HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(RuntimeException.class)
+	public ResponseEntity<?> contactDetailsExceptionHandler(RuntimeException e){
+		return new ResponseEntity<>(new ErrorResponse("Something went wrong", e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 }
