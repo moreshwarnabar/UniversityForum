f4aeec201b1c3986cd89a60493561a1e52d7b8d5
Moreshwar Nabar <mrnabar@gmail.com>
2021-03-08 11:37:06 +0530

    handle exceptions for subscription and category

--- QuoraClone/src/main/java/com/app/exceptionhandler/GlobalExceptionHandler.java
+++ QuoraClone/src/main/java/com/app/exceptionhandler/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.app.customexception.CategoryNotFoundException;
 import com.app.customexception.UserAuthorizationException;
 import com.app.customexception.UserNotFoundException;
 import com.app.dto.ErrorResponse;
@@ -23,6 +24,9 @@ public class GlobalExceptionHandler {
 		return new ResponseEntity<>(new ErrorResponse("User not found", e.getMessage()), HttpStatus.NOT_FOUND);
 	}
 	
-	// exception handler methods
+	@ExceptionHandler(CategoryNotFoundException.class)
+	public ResponseEntity<?> categoryExceptionHandler(CategoryNotFoundException e) {
+		return new ResponseEntity<>(new ErrorResponse("Category not found", e.getMessage()), HttpStatus.NOT_FOUND);
+	}
 
 }
